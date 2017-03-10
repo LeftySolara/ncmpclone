0b5020f7320dc5c047c99c4bc34926db781c4594
Jalen Adams <leftysolara@gmail.com>
2017-03-10 17:50:13 -0500

    add mpd connection to test that library is working

--- main.c
+++ main.c
@@ -1,6 +1,24 @@
 #include <ncurses.h>
+#include <mpd/client.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#define MPD_DEFAULT_HOST "localhost"
+#define MPD_DEFAULT_PORT 6600
+#define MPD_DEFAULT_TIMEOUT 5000
 
 int main() {
+
+    /* Establish a connection with the local MPD server */
+    struct mpd_connection *mpd_conn = mpd_connection_new(MPD_DEFAULT_HOST,
+                                                         MPD_DEFAULT_PORT,
+                                                         MPD_DEFAULT_TIMEOUT);
+
+    if (mpd_connection_get_error(mpd_conn) != MPD_ERROR_SUCCESS) {
+        printf("Error connecting to MPD server\n");
+        mpd_connection_free(mpd_conn);
+        exit(1);
+    }
 
     /* Initialize ncurses */
     initscr();
@@ -12,6 +30,7 @@ int main() {
 
     int x = getmaxx(stdscr) / 2;
     int y = getmaxy(stdscr) / 2;
+    mvprintw(LINES - 1, 0, "Press F1 to exit");
     mvprintw(y, x, "Hello world!");
     refresh();
 
@@ -21,6 +40,7 @@ int main() {
     }
 
     endwin();
+    mpd_connection_free(mpd_conn);
 
     return 0;
 }
