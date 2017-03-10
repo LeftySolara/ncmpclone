d1ca5598b4450aa9e5c461763da7aa7ab097107d
Jalen Adams <leftysolara@gmail.com>
2017-03-10 18:48:58 -0500

    add license

--- main.c
+++ main.c
@@ -1,3 +1,26 @@
+/******************************************************************************
+ * main.c : application entry point
+ * ****************************************************************************
+ * Copyright (C) 2017 Jalen Adams
+ *
+ * Authors: Jalen Adams <leftysolara@gmail.com>
+ *
+ * This file is part of ncmpclone.
+ *
+ * ncmpclone is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * ncmpclone is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with ncmpclone.  If not, see <http://www.gnu.org/licenses/>.
+ ***************************************************************************/
+
 #include <ncurses.h>
 #include <mpd/client.h>
 #include <stdio.h>
