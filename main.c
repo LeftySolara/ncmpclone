842e0aa96c5e7c699565faadefebb5f07bc2600e
Jalen Adams <leftysolara@gmail.com>
2017-05-20 10:39:29 -0400

    add keybinds for playing previous and next song

--- main.c
+++ main.c
@@ -124,6 +124,12 @@ int main(int argc, char *argv[]) {
             case 's':
                 mpd_run_stop(mpd_info->connection);
                 break;
+            case '<':
+                mpd_run_previous(mpd_info->connection);
+                break;
+            case '>':
+                mpd_run_next(mpd_info->connection);
+                break;
             case 'z':
                 mpd_run_random(mpd_info->connection, !mpd_status_get_random(mpd_info->status));
                 status_bar->notification = !mpd_status_get_random(mpd_info->status) ?
